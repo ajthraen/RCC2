748daca59c34e278bb3b9d72b52f38e5dcecb723
AJ <ajthraen@gmail.com>
2022-09-22 21:28:12 -0400

    RCC reload5

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,27 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
 function Home() {
+    const [users, setUsers] = useState([])
+
+    async function fetchUsers() {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+        setUsers(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
     return (
         <div className="header">
-            <h1>Home</h1>
+            {users.length > 0
+                ? <h1>{users.length > 0 && users[0].name}</h1>
+                : <h1>Loading...</h1>
+            }
         </div>
-    )
+    );
 }
 
 export default Home;
